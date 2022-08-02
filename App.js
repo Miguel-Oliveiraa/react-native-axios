f784a0058e7c50f68d28795627250137fe21734d
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-08-01 23:52:43 -0300

    feat: styling the components

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import api from "./src/services/axios";
 
 export default function App() {
+  const [users, setUser] = useState();
+
+  useEffect(() => {
+    api
+      .get("/users")
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}
+    >
+      <Image source={require(`../my-app/assets/imgs/1.png`)} />
+      {/* <Text>{typeof item.id}</Text> */}
+      <View style={{ marginLeft: 18 }}>
+        <Text style={{ fontWeight: "600", fontSize: 16 }}>{item.name}</Text>
+        <Text style={{ fontWeight: "400", fontSize: 12 }}>{item.username}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {console.log(typeof users)}
+      <Text style={{ fontWeight: "700", fontSize: 32, marginBottom: 20 }}>
+        Friends
+      </Text>
+      <FlatList
+        data={users}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +54,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: 60,
+    marginLeft: 20,
   },
 });
